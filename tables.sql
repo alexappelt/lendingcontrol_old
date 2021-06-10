8263d0b5002eba5cdbbaf4e71c11aa8f3d3abfb9
GuiBrust <gbrust15@gmail.com>
2021-06-10 12:39:14 -0300

    login ajustes

--- tables.sql
+++ tables.sql
@@ -15,15 +15,15 @@ insert into status values(1, 'Disponível'), (2, 'Emprestado'), (3, 'Reservado')
 
 
 CREATE SEQUENCE seq_pk_clientes START 1;
-
 create table clientes (
  id int primary key  DEFAULT nextval('seq_pk_clientes'),
- nome varchar(40),
- sobrenome varchar(40),
+ nome varchar(80),
  idade integer,
  usuario varchar(40),
- senha varchar(40)
+ senha varchar(40),
+ root boolean 
  );
+INSERT INTO clientes (nome, idade, usuario, senha, root) VALUES ('admin admin',	99, 'admin', '21232f297a57a5a743894a0e4a801fc3', true);
 
 
 CREATE SEQUENCE seq_pk_produtos START 1;
@@ -39,4 +39,17 @@ create table produtos (
 );
 
 
+create sequence seq_pk_emprestimos start 1;
+create table emprestimos (
+	id int primary key default nextval('seq_pk_emprestimos'),
+	id_mutuante integer,
+	id_benificiario integer,
+	id_produto integer,
+	data date,
+	foreign key (id_mutuante) references clientes(id),
+	foreign key (id_benificiario) references clientes(id),
+	foreign key (id_produto) references produtos(id)
+);
+
+select * from clientes c 
 
